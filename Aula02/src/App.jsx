18a2dd9d28a2e68504ac4622abad862b392c6e63
Kauan Bomfim <111811804+wkauan@users.noreply.github.com>
2023-08-23 21:33:55 -0300

    ✨ Feat: Mexendo com componentes em React

---
+++ Aula02/src/App.jsx
@@ -0,0 +1,22 @@
+import FirstComponent from './components/FirstComponent'
+import TemplateExpressions from './components/TemplateExpressions'
+import MyComponent from './components/MyComponent'
+import Events from './components/Events'
+import Challenge from './components/Challenge'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Fundamentos React</h1>
+      <FirstComponent />
+      <TemplateExpressions />
+      <MyComponent />
+      <Events />
+      <Challenge />
+    </>
+  )
+}
+
+export default App
---
+++ Aula02/src/components/TemplateExpressions.jsx
@@ -0,0 +1,16 @@
+export default function TemplateExpressions() {
+    const name = "Kauan"
+    const data = {
+        age: 31,
+        job: "Programmer",
+    };
+
+    return (
+        <div>
+            <h1>Olá {name}, tudo bem?</h1>
+            <p>Você atua como {data.job}</p>
+            <p>{4+4}</p>
+            <p>{console.log("JSX React")}</p>
+        </div>
+    )
+}
